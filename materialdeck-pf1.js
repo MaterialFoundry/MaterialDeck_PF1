0b55f9c77200e615130133815444679d8832b548
Material Foundry <github@materialfoundry.nl>
2024-05-11 00:13:35 +0200

    v1.0.1

--- materialdeck-pf1.js
+++ materialdeck-pf1.js
@@ -1,5 +1,3 @@
-import { materialDeck } from "../MaterialDeck/MaterialDeck.js";
-
 const data = {
     moduleId: 'materialdeck-pf1',
     systemId: 'pf1',
@@ -336,7 +334,7 @@ class system {
 Hooks.once('MaterialDeck_Ready', () => {
     const moduleData = game.modules.get(data.moduleId);
 
-    materialDeck.registerSystem({
+    game.materialDeck.registerSystem({
         systemId: data.systemId,
         moduleId: data.moduleId,
         systemName: data.systemName,
